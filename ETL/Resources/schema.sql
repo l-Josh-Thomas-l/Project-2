8fd8a9e95dc67338d6503b3360576ec2eac158d3
KFerr22 <101094352+KFerr22@users.noreply.github.com>
2022-07-24 16:59:32 +0800

    ERD documents added

---
+++ ETL/Resources/schema.sql
@@ -0,0 +1,53 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Country" (
+    "Country" VARCHAR   NOT NULL,
+    "Continent" VARCHAR   NOT NULL,
+    "Abbreviation_2" VARCHAR   NOT NULL,
+    "Abbreviation_3" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_Country" PRIMARY KEY (
+        "Country"
+     )
+);
+
+CREATE TABLE "Production" (
+    "Country" VARCHAR   NOT NULL,
+    "Yield_type" VARCHAR   NOT NULL,
+    "Year" INTEGER   NOT NULL,
+    "Value(kg/ha)" NUMERIC   NOT NULL,
+    "Continent" VARCHAR   NOT NULL,
+    "Abbreviation_2" VARCHAR   NOT NULL,
+    "Abbreviation_3" VARCHAR   NOT NULL
+);
+
+CREATE TABLE "GDP" (
+    "Country" VARCHAR   NOT NULL,
+    "Year" INTEGER   NOT NULL,
+    "GDP_per_capita" NUMERIC   NOT NULL,
+    "Continent" VARCHAR   NOT NULL,
+    "Abbreviation_2" VARCHAR   NOT NULL,
+    "Abbreviation_3" VARCHAR   NOT NULL
+);
+
+CREATE TABLE "Fertilizer" (
+    "Country" VARCHAR   NOT NULL,
+    "Year" INTEGER   NOT NULL,
+    "Nitrogen(kg/ha)" NUMERIC   NOT NULL,
+    "Potash(kg/ha)" NUMERIC   NOT NULL,
+    "Phosphate(kg/ha)" NUMERIC   NOT NULL,
+    "Continent" VARCHAR   NOT NULL,
+    "Abbreviation_2" VARCHAR   NOT NULL,
+    "Abbreviation_3" VARCHAR   NOT NULL
+);
+
+ALTER TABLE "Production" ADD CONSTRAINT "fk_Production_Country" FOREIGN KEY("Country")
+REFERENCES "Country" ("Country");
+
+ALTER TABLE "GDP" ADD CONSTRAINT "fk_GDP_Country" FOREIGN KEY("Country")
+REFERENCES "Country" ("Country");
+
+ALTER TABLE "Fertilizer" ADD CONSTRAINT "fk_Fertilizer_Country" FOREIGN KEY("Country")
+REFERENCES "Country" ("Country");
+
